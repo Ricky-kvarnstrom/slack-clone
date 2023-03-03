a5ff00c5d8487dc28c10d3d449c15cc559258532
Ricky K <riickyy_kvarn@hotmail.se>
2023-03-03 19:15:26 +0100

    added styling for sidebar

--- server/index.js
+++ server/index.js
@@ -1,8 +1,34 @@
 import express from "express";
 const app = express();
+import mongoose from "mongoose";
+import cors from "cors";
+
+app.use(cors({ origin: "*" }));
 
 app.get("/", (req, res) => {
   res.send("ricky");
 });
 
-app.listen(3000);
+const channelsShema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 30,
+    },
+  },
+  { timestamp: true }
+);
+const Channel = mongoose.model("channels", channelsShema);
+
+app.get("/channels", async (req, res) => {
+  const channels = await Channel.find();
+  res.send(channels);
+});
+
+app.listen(3000, async () => {
+  await mongoose.connect(
+    "mongodb+srv://ricky94:ricky94@cluster0.mbsozhx.mongodb.net/?retryWrites=true&w=majority"
+  );
+});
